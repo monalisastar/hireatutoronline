7746096b6186e9542f7761b7139a6ed4d54b82e8
monalisastar <investmentssilverdollar@gmail.com>
2025-06-27 22:43:11 +0300

    Fix Bing meta tag using app/head.tsx

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+// app/head.tsx
+
+export default function Head() {
+  return (
+    <>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="msvalidate.01" content="16822ECC461C2545289B082943C89638" />
+      <title>Hire A Tutor | Expert Tutoring Platform</title>
+    </>
+  );
+}
+
